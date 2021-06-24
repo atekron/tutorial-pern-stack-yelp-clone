26f8b5dcf8e20030166e1d5a3f0337232bc42e4a
Michael Kret <dirtysculptor@gmail.com>
2021-06-24 21:20:28 +0300

    configuring express

---
+++ server/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+//get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: ["mcdonalds", "burger king"],
+    },
+  });
+});
+
+//get a restaurant
+app.get("/api/v1/restaurants/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+//create a restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+  console.log(req.body);
+});
+
+const port = process.env.PORT || 3005;
+app.listen(port, () => {
+  console.log(`server is up and listening on port ${port}`);
+});
