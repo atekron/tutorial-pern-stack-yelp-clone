5d5abc2764a93d44453a5b50ed3aa300621dbf53
Michael Kret <dirtysculptor@gmail.com>
2021-06-25 19:58:41 +0300

    react routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./routes/HomePage";
+import RestaurantDetailsPage from "./routes/RestaurantDetailsPage";
+import RestaurantUpdate from "./routes/RestaurantUpdate";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route
+            exact
+            path="/restaurants/:id/update"
+            component={RestaurantUpdate}
+          />
+          <Route
+            exact
+            path="/restaurants/:id"
+            component={RestaurantDetailsPage}
+          />
+        </Switch>
+      </Router>
     </div>
   );
 }
