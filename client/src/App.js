5b9da27b96303928016123a11c7a951985524e94
Michael Kret <dirtysculptor@gmail.com>
2021-06-26 07:25:44 +0300

    context provider setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,29 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./routes/HomePage";
 import RestaurantDetailsPage from "./routes/RestaurantDetailsPage";
 import RestaurantUpdate from "./routes/RestaurantUpdate";
+import { RestaurantsContextProvider } from "./context/RestaurantsContext";
 
 function App() {
   return (
-    <div className="container">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route
-            exact
-            path="/restaurants/:id/update"
-            component={RestaurantUpdate}
-          />
-          <Route
-            exact
-            path="/restaurants/:id"
-            component={RestaurantDetailsPage}
-          />
-        </Switch>
-      </Router>
-    </div>
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route
+              exact
+              path="/restaurants/:id/update"
+              component={RestaurantUpdate}
+            />
+            <Route
+              exact
+              path="/restaurants/:id"
+              component={RestaurantDetailsPage}
+            />
+          </Switch>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
   );
 }
 
